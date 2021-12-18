200264284819fc4f9fd8a9072a6f36c59cd349d6
tpylkko <33058625+tpylkko@users.noreply.github.com>
2021-12-18 19:24:42 +0200

    Update levyttäjä.jl

--- levyttäjä.jl
+++ levyttäjä.jl
@@ -1,25 +1,24 @@
 using ArgParse
 s = ArgParseSettings()
 @add_arg_table! s begin
-    "arg1"
-    arg_type = Int
-    default = 5
-    help = "Specify the column of the csv for input"
-    required = true
-    "arg2"
-    arg_type = Int
-    default = 96
-    help = "Is the data from 96 or 384 well plate"
-    required = true
-    "arg3"
-    arg_type = String
-    default = "."
-    help = "name of the file"
-    required = true
+    "column"
+        arg_type = Int
+        default = 5
+        help = "Specify the column of the csv for input"
+        required = true
+    "--384"
+        help = "If the plate is 384-well use this flag. Otherwise
+            presumed to be 96-well"
+        action = :store_true
+    "filename"
+        arg_type = String
+        default = "."
+        help = "name of the file"
+        required = true
 end
 
 parsed_args = parse_args(ARGS, s)
-
+@info "Arguments parsed, loading libraries and file..."
 using NaturalSort, CSV, DataFrames
 
 struct Plate
@@ -27,18 +26,23 @@ struct Plate
     column::Int
 end
 
-if parsed_args["arg2"] == 96
+if parsed_args["384"] == false
     plate = Plate(8,12)
 end
-if parsed_args["arg2"] == 384
+if parsed_args["384"] == true
     plate = Plate(16,24)
 end
 
 
 # which column are you interested in. Sort requires DataFrame
-const interesting_column = parsed_args["arg1"]
-data = CSV.File(string(parsed_args["arg3"])) |> DataFrame
+const interesting_column = parsed_args["column"]
+if isfile(parsed_args["filename"]) == false
+    @error "The file does not exist"
+    exit(86)
+end
+data = CSV.File(string(parsed_args["filename"])) |> DataFrame
 sort!(data, [:well], lt=natural)
+@info "File found, values sorted"
 
 # a demonstratoin of how a dataframe can be transposed already at init
 #data_t = CSV.File("trims.csv", transpose=true) |> DataFrame
@@ -65,6 +69,7 @@ end
 df = levymuotoon(interesting_column)
 df = DataFrame(df, :auto)
 #scatter(data.Column1,data[5], xticks=4)
-file_location = splitdir(parsed_args["arg3"])
+file_location = splitdir(parsed_args["filename"])
 output_filename = string(file_location[1], "/ped_data_plateformat.csv")
 CSV.write(output_filename, df)
+@info "Done! The output has been written to file in the same path"
